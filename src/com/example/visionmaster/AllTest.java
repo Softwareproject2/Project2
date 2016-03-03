263bc6596a39f263a85c4f846b7f4b5e44f1503f
albertmonteiro <albertmonteiro@live.com>
2016-03-03 18:16:09 -0500

    Test score database done

--- src/com/example/visionmaster/AllTest.java
+++ src/com/example/visionmaster/AllTest.java
@@ -20,7 +20,14 @@ public class AllTest extends Activity implements OnClickListener {
 		setContentView(R.layout.alltest);
 		colortest = (ImageButton) findViewById(id.colorTest);
 		colortest.setOnClickListener(this);
-		
+		visualacuity = (ImageButton) findViewById(id.visualAcuity);
+		visualacuity.setOnClickListener(this);
+		cameraApplication = (ImageButton) findViewById(id.cameraApplication);
+		cameraApplication.setOnClickListener(this);
+		questions = (ImageButton) findViewById(R.id.questions);
+		questions.setOnClickListener(this);
+		motorTest = (ImageButton) findViewById(R.id.motorTest);
+		motorTest.setOnClickListener(this);
 	}
 
 	@Override
@@ -32,7 +39,23 @@ public class AllTest extends Activity implements OnClickListener {
 			intent = new Intent(AllTest.this, FirstPage.class);
 			startActivity(intent);
 			break;
-				default:
+		case R.id.visualAcuity:
+			intent = new Intent(AllTest.this, VisualAcuity.class);
+			startActivity(intent);
+			break;
+//		case R.id.cameraApplication:
+//			intent = new Intent(AllTest.this, CameraActivity.class);
+//			startActivity(intent);
+//			break;
+		case R.id.questions:
+			intent = new Intent(AllTest.this, Question1.class);
+			startActivity(intent);
+			break;
+//		case R.id.motorTest:
+//			intent = new Intent(AllTest.this, MotorTest.class);
+//			startActivity(intent);
+//			break;
+		default:
 			break;
 		}
 	}
@@ -42,5 +65,15 @@ public class AllTest extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		super.onPause();
 		finish();
+	}
+
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		super.onBackPressed();
+		Intent intent = new Intent(AllTest.this, LoginScreen.class);
+		startActivity(intent);
+		Toast.makeText(getApplicationContext(), "You are logged out.", 0)
+				.show();
 	}
 }
---
+++ src/com/example/visionmaster/LoginScreen.java
@@ -0,0 +1,181 @@
+package com.example.visionmaster;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.visionmaster.R.id;
+
+public class LoginScreen extends Activity {
+	LinearLayout linearlayout;
+	Button saveandsubmit, settings;
+	TextView texttitle;
+	LayoutInflater inflater;
+	static String EMAIL = "physiciansid@gmail.com";
+	public static SharedPreferences shared;
+	public static EditText physicianid, patientId;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loginscreen);
+
+		// set the custom Text
+		Typeface tf = Typeface.createFromAsset(getAssets(),
+				"fonts/DroidSerif-BoldItalic.ttf");
+		shared = getSharedPreferences("EyeTest", MODE_WORLD_WRITEABLE);
+		final SharedPreferences.Editor spedit = shared.edit();
+		texttitle = (TextView) findViewById(id.textTitle);
+		texttitle.setTypeface(tf);
+		saveandsubmit = (Button) findViewById(id.saveandsubmit);
+		settings = (Button) findViewById(id.settings);
+		physicianid = (EditText) findViewById(id.physicianid);
+		patientId = (EditText) findViewById(id.patientId);
+		settings.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				inflater = LayoutInflater.from(getBaseContext());
+				View view = inflater.inflate(R.layout.settinginflate, null);
+				final Dialog dialog = new Dialog(LoginScreen.this);
+				dialog.setContentView(view);
+				final EditText newpassfield=(EditText)view. findViewById(id.newpassfield);
+				final Button savepass = (Button)view. findViewById(id.savenewpassword);
+				savepass.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						spedit.putString("PATIENT_ID", newpassfield.getText()
+								.toString().trim());
+						spedit.commit();
+						patientId.setText(shared.getString("PATIENT_ID", ""));
+						dialog.dismiss();
+					}
+				});
+				dialog.show();
+			}
+		});
+
+		patientId.setText(shared.getString("PATIENT_ID", ""));
+		physicianid.setText(shared.getString("PHYSICIAN_ID", ""));
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(patientId.getWindowToken(), 0);
+		imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(physicianid.getWindowToken(), 0);
+		if (patientId.getText().toString()
+				.equals(shared.getString("PATIENT_ID", ""))) {
+			patientId.setEnabled(false);
+			physicianid.setEnabled(false);
+			saveandsubmit.setText("Login");
+		}
+		if (patientId.getText().toString().equals("")
+				&& physicianid.getText().toString().equals("")) {
+			patientId.setEnabled(true);
+			physicianid.setEnabled(true);
+			saveandsubmit.setText("Save");
+		} else {
+			// do nothing
+		}
+		saveandsubmit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// saveandsubmit.setBackgroundDrawabl8q e(null);
+				if (saveandsubmit.getText().toString().equals("Save")) {
+					if (patientId.getText().toString().equals("")
+							|| physicianid.getText().toString().equals("")
+							|| !physicianid.getText().toString().equals(EMAIL)) {
+						Dialog dialog = new Dialog(LoginScreen.this);
+						dialog.setTitle("Please enter correct id's");
+						dialog.show();
+					} else {
+						spedit.putString("PATIENT_ID", patientId.getText()
+								.toString().trim());
+						spedit.putString("PHYSICIAN_ID", physicianid.getText()
+								.toString().trim());
+						spedit.commit();
+
+						AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+								LoginScreen.this);
+						// set title
+
+						// set dialog message
+						alertDialogBuilder
+								.setMessage(
+										"Do you want to provide permission to record your location?")
+								.setCancelable(false)
+								.setPositiveButton("Yes",
+										new DialogInterface.OnClickListener() {
+											public void onClick(
+													DialogInterface dialog,
+													int id) {
+												Intent intent = new Intent(
+														LoginScreen.this,
+														MainPage.class);
+												startActivity(intent);
+											}
+										})
+								.setNegativeButton("No",
+										new DialogInterface.OnClickListener() {
+											public void onClick(
+													DialogInterface dialog,
+													int id) {
+												// if this button is clicked,
+												// just close
+												// the dialog box and do nothing
+												Intent intent = new Intent(
+														LoginScreen.this,
+														MainPage.class);
+												startActivity(intent);
+												dialog.cancel();
+											}
+										});
+
+						// create alert dialog
+						AlertDialog alertDialog = alertDialogBuilder.create();
+
+						// show it
+						alertDialog.show();
+
+					}
+				} else if (saveandsubmit.getText().toString().equals("Login")) {
+					Intent intent = new Intent(LoginScreen.this, MainPage.class);
+					startActivity(intent);
+				} else {
+
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+
+}
